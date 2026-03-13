f46388fb55b9afa84620a4457924d42ff229370a
JEYABENSIA <bensia17062006@gmail.com>
2026-03-13 14:56:20 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,35 @@
+package com.student;
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(
+                "select * from users where username=? and password=?"
+            );
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                res.sendRedirect("dashboard.jsp");
+            } else {
+                res.getWriter().println("Invalid Login");
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
